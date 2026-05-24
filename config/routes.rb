410a2de9df5f4b393a32929141abe4b37ce8b433
Nikita Popov <losnikitos@gmail.com>
2026-05-24 17:33:26 +0100

    Main page

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  root "projects#index"
+  root "home#index"
 
   resources :projects, only: %i[index] do
     collection do
