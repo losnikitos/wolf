1230f2ae732277f8ae6484034693c950261fc24e
Nikita Popov <losnikitos@gmail.com>
2026-05-31 14:41:08 +0100

    Fav projects on main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @favorite_projects = Project.active.favorites.includes(:client).order(year: :desc, name: :asc).limit(4)
+    @favorite_projects = Project.active.favorites.includes(:client).order(year: :desc, name: :asc)
   end
 end
